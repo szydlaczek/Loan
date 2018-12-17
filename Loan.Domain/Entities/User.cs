06e0ad8865cf686da64867909c5f1026c78445b5
Grzegorz Szydło <szydlo.grzegorz@outlook.com>
2018-12-17 21:26:09 +0100

    Create project

---
+++ Loan.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Loan.Domain.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
